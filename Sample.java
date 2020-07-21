acb62a142de8bb4dcfd709be75ed8933f72dd2d9
Govind Choudhary <gchoudhary@govindch-ltm.internal.salesforce.com>
2020-07-21 15:58:01 +0530

    java

---
+++ Sample.java
@@ -0,0 +1,9 @@
+public class Sample{
+    public static void main(String[] args)
+    {
+        for(int i=0;i<10;i=i+1)
+        {
+            System.out.printf("hello world %d\n",i);
+        }
+    }
+}
